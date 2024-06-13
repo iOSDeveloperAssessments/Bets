b4ee5278169a1be1085485d42451361071975005
David <momachilles@gmail.com>
2024-06-13 10:26:59 +0200

    Add variation property.

--- BetsCore/Bet.swift
+++ BetsCore/Bet.swift
@@ -1,11 +1,15 @@
-public struct Bet {
-    public var name: String
-    public var sellIn: Int
-    public var quality: Int
+public typealias OddVariation = (sellIn: Int, quality: Int)
 
-    public init(name: String, sellIn: Int, quality: Int) {
-        self.name = name
-        self.sellIn = sellIn
-        self.quality = quality
-    }
+public struct Bet {
+  public var name: String
+  public var sellIn: Int
+  public var quality: Int
+  public var variation: OddVariation
+  
+  public init(name: String, sellIn: Int, quality: Int, variation: OddVariation = (.zero, .zero)) {
+    self.name = name
+    self.sellIn = sellIn
+    self.quality = quality
+    self.variation = variation
+  }
 }
---
+++ BetsCore/OddsUpdater.swift
@@ -0,0 +1,8 @@
+//
+//  OddsUpdater.swift
+//  BetsCore
+//
+//  Created by David Alarcon on 13/6/24.
+//
+
+import Foundation
