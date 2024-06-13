df2726246a578d9c3b505df5e10c8de495fc88d5
David <momachilles@gmail.com>
2024-06-13 11:52:15 +0200

    Add action for two first bets.

--- Bets/Service/RemoteBetService.swift
+++ Bets/Service/RemoteBetService.swift
@@ -6,8 +6,7 @@ actor RemoteBetService: BetService {
     public static let instance = RemoteBetService()
     
     private init() { }
-    
-  /*
+    /*
     private var bets: [Bet] = [
         Bet(name: "Winning team", sellIn: 8, quality: 15),
         Bet(name: "Total score", sellIn: 5, quality: 26),
@@ -25,26 +24,33 @@ actor RemoteBetService: BetService {
         Bet(name: "Number of sets won", sellIn: 8, quality: 17),
         Bet(name: "Number of aces", sellIn: 6, quality: 46),
         Bet(name: "Set score", sellIn: 9, quality: 12),
-    ]
-   */
+    ] */
   
   private var bets: [Bet] = [
-    Bet(name: "Winning team", sellIn: 8, quality: 15, variation: (.zero, .zero)),
-    Bet(name: "Total score", sellIn: 5, quality: 26, variation: (.zero, .zero)),
-    Bet(name: "Player performance", sellIn: 9, quality: 4, variation: (.zero, .zero)),
-    Bet(name: "First goal scorer", sellIn: 10, quality: 49, variation: (.zero, .zero)),
-    Bet(name: "Number of fouls", sellIn: 4, quality: 21, variation: (.zero, .zero)),
-    Bet(name: "Corner kicks", sellIn: 9, quality: 32, variation: (.zero, .zero)),
-    Bet(name: "Yellow cards", sellIn: 0, quality: 45, variation: (.zero, .zero)),
-    Bet(name: "Red cards", sellIn: 2, quality: 11, variation: (.zero, .zero)),
-    Bet(name: "Offsides", sellIn: 2, quality: 14, variation: (.zero, .zero)),
-    Bet(name: "Penalties", sellIn: 4, quality: 10, variation: (.zero, .zero)),
-    Bet(name: "Half-time score", sellIn: 1, quality: 42, variation: (.zero, .zero)),
-    Bet(name: "Clean sheet", sellIn: 10, quality: 34, variation: (.zero, .zero)),
-    Bet(name: "Hat trick", sellIn: 9, quality: 32, variation: (.zero, .zero)),
-    Bet(name: "Number of sets won", sellIn: 8, quality: 17, variation: (.zero, .zero)),
-    Bet(name: "Number of aces", sellIn: 6, quality: 46, variation: (.zero, .zero)),
-    Bet(name: "Set score", sellIn: 9, quality: 12, variation: (.zero, .zero)),
+    Bet(name: "Winning team", sellIn: 8, quality: 15),
+    Bet(name: "Total score", sellIn: 5, quality: 26) { bet in
+      if bet.quality < 50 {
+        bet.quality = bet.quality + 1
+        if bet.sellIn < 11 && bet.quality < 50 { bet.quality = bet.quality + 1 }
+        if bet.sellIn < 6 && bet.quality < 50 { bet.quality = bet.quality + 1 }
+      }
+      bet.sellIn = bet.sellIn - 1
+      if bet.sellIn < 0 { bet.quality = bet.quality - bet.quality }
+    },
+    Bet(name: "Player performance", sellIn: 9, quality: 4),
+    Bet(name: "First goal scorer", sellIn: 10, quality: 49),
+    Bet(name: "Number of fouls", sellIn: 4, quality: 21),
+    Bet(name: "Corner kicks", sellIn: 9, quality: 32),
+    Bet(name: "Yellow cards", sellIn: 0, quality: 45),
+    Bet(name: "Red cards", sellIn: 2, quality: 11),
+    Bet(name: "Offsides", sellIn: 2, quality: 14),
+    Bet(name: "Penalties", sellIn: 4, quality: 10),
+    Bet(name: "Half-time score", sellIn: 1, quality: 42),
+    Bet(name: "Clean sheet", sellIn: 10, quality: 34),
+    Bet(name: "Hat trick", sellIn: 9, quality: 32),
+    Bet(name: "Number of sets won", sellIn: 8, quality: 17),
+    Bet(name: "Number of aces", sellIn: 6, quality: 46),
+    Bet(name: "Set score", sellIn: 9, quality: 12),
   ]
     
     func loadBets() async throws -> [Bet] {
