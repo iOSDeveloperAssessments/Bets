82b6fa673e611ec923edae9aa024ea52f98c8015
David <momachilles@gmail.com>
2024-06-13 10:27:53 +0200

    Add updater class.

--- BetsCore/OddsUpdater.swift
+++ BetsCore/OddsUpdater.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+protocol Updater {
+  associatedtype Updatable
+  static func update(_ updatable: Updatable) -> Updatable
+}
+
+class OddsUpdater: Updater {
+  static func update(_ updatable: Bet) -> Bet {
+    .init(name: updatable.name,
+          sellIn: updatable.sellIn + updatable.variation.sellIn,
+          quality: updatable.quality + updatable.variation.quality,
+          variation: updatable.variation)
+  }
+}
