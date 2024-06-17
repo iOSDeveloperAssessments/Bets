9a065972a772905f0883cbf7cde82cbb6c761ef5
David <momachilles@gmail.com>
2024-06-17 09:29:17 +0200

    Delete commented code.

--- BetsCore/BetsRepository.swift
+++ BetsCore/BetsRepository.swift
@@ -15,63 +15,6 @@ public class BetRepository: Repository {
     self.service = service
     self.oddsUpdater = updater
   }
-/*
-    public func updateOdds() async throws -> [Bet] {
-        var bets = try await service.loadBets()
-        
-        for i in 0 ..< bets.count {
-            if bets[i].name != "Player performance", bets[i].name != "Total score" {
-                if bets[i].quality > 0 {
-                    if bets[i].name != "Winning team" {
-                        bets[i].quality = bets[i].quality - 1
-                    }
-                }
-            } else {
-                if bets[i].quality < 50 {
-                    bets[i].quality = bets[i].quality + 1
-
-                    if bets[i].name == "Total score" {
-                        if bets[i].sellIn < 11 {
-                            if bets[i].quality < 50 {
-                                bets[i].quality = bets[i].quality + 1
-                            }
-                        }
-
-                        if bets[i].sellIn < 6 {
-                            if bets[i].quality < 50 {
-                                bets[i].quality = bets[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if bets[i].name != "Winning team" {
-                bets[i].sellIn = bets[i].sellIn - 1
-            }
-
-            if bets[i].sellIn < 0 {
-                if bets[i].name != "Player performance" {
-                    if bets[i].name != "Total score" {
-                        if bets[i].quality > 0 {
-                            if bets[i].name != "Winning team" {
-                                bets[i].quality = bets[i].quality - 1
-                            }
-                        }
-                    } else {
-                        bets[i].quality = bets[i].quality - bets[i].quality
-                    }
-                } else {
-                    if bets[i].quality < 50 {
-                        bets[i].quality = bets[i].quality + 1
-                    }
-                }
-            }
-        }
-
-        try await service.saveBets(bets)
-        return bets
-    } */
   
   public func updateOdds() async throws -> [Bet] {
     var bets = try await service.loadBets()
